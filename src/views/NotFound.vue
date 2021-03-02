b3f0f082beecb5f9938c4f40553fd89b11df9c59
yaoyuan <xiaocaotx@126.com>
2021-03-02 15:05:29 +0800

    添加404页

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <div>当前网页不存在，请检查网络是否正常</div>
+  <router-link to="/">返回首页</router-link>
+</div>
+</template>
+
+<script lang = "ts">
+export default {
+name: "NotFound"
+}
+</script >
+
+<style lang = 'scss' scoped>
+
+</style>
