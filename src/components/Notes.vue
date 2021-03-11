6591e20d0e4ad816d3ac954c9bf1b6036fec9e1a
yaoyuan <xiaocaotx@126.com>
2021-03-11 17:51:09 +0800

    创建money页面的各个组件，然后layout添加props来给money控制大布局

---
+++ src/components/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里添加备注">
+    </label>
+  </div>
+</template>
+
+<script lang = "ts">
+export default {
+name: "Notes"
+}
+</script >
+
+<style lang = 'scss' scoped>
+@import  "~@/assets/styles/helper.scss";
+.notes{
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name{
+    padding-right: 16px;
+  }
+  input{
+    height: 64px;
+    padding-right: 16px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+  }
+}
+</style>
---
+++ src/components/Tags.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tags">
+    <ul class="current-tag">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+  </div>
+</template>
+
+<script lang = "ts">
+export default {
+name: "Tags"
+}
+</script >
+
+<style lang = 'scss' scoped>
+@import  "~@/assets/styles/helper.scss";
+.tags{
+  font-size: 14px;
+  padding: 16px;
+  > .current-tag{
+    display: flex;
+    > li{
+      background: #d9d9d9;
+      $h:24px;
+      height: $h;
+      line-height: $h;
+      border-radius: ($h/2);
+      padding: 0 16px;
+      margin-right: 12px;
+    }
+  }
+  > .new{
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+
+}
+
+</style>
