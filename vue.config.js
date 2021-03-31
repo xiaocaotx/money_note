a213a3ce4b0ac507979bd80bca472224904a73df
遥远 <xiaocaotx@126.com>
2021-03-31 14:42:23 +0800

    vue.config

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+const path = require('path')
+
+module.exports = {
+
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/money-note-website/'
+  : '/',
+
+  lintOnSave: false,
+  chainWebpack:config =>{
+    const  dir = path.resolve(__dirname,'src/assets/icons')
+    config.module
+        .rule('svg-sprite')
+        .test(/\.svg$/)
+        .include.add(dir).end()
+        .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
+        .use('svgo-loader').loader('svgo-loader')
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir)
+
+  }
+
+}
